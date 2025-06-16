2602cbc974fd961a7648372985de16b66e52da87
Log <nguyennamlong0411@gmail.com>
2025-06-16 13:41:23 +0700

    Bo  CartDetail va InvoiceDetail

--- src/java/model/Cart.java
+++ src/java/model/Cart.java
@@ -4,10 +4,68 @@
  */
 package model;
 
+import java.sql.Date;
+
 /**
  *
  * @author HP
  */
-public class Cart {
+public class Cart{
+    private int cartID ;
+    private String userID;
+    private Date createDate;
+    private int productID;
+    private int quantity;
+    
+    public Cart() {
+    }
+
+    public Cart(int cartID, String userID, Date createDate, int productID, int quantity) {
+        this.cartID = cartID;
+        this.userID = userID;
+        this.createDate = createDate;
+        this.productID = productID;
+        this.quantity = quantity;
+    }
+
+    public int getCartID() {
+        return cartID;
+    }
+
+    public void setCartID(int cartID) {
+        this.cartID = cartID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
     
 }
--- src/java/model/Invoice.java
+++ src/java/model/Invoice.java
@@ -4,10 +4,97 @@
  */
 package model;
 
+import java.sql.Date;
+
 /**
  *
  * @author HP
  */
 public class Invoice {
-    
+ private int invoiceID;
+ private String userID;
+ private float totalAmount;
+ private String status;
+ private Date createDate;
+ private int productID;
+ private int quantity;
+ private float price; 
+
+    public Invoice() {
+    }
+
+    public Invoice(int invoiceID, String userID, float totalAmount, String status, Date createDate, int productID, int quantity, float price) {
+        this.invoiceID = invoiceID;
+        this.userID = userID;
+        this.totalAmount = totalAmount;
+        this.status = status;
+        this.createDate = createDate;
+        this.productID = productID;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getInvoiceID() {
+        return invoiceID;
+    }
+
+    public void setInvoiceID(int invoiceID) {
+        this.invoiceID = invoiceID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public float getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(float totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
